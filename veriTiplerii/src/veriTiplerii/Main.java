68c4362d6a67d2c0a83e8b8a159f4fbff600f57e
emekciceyhun <47597251+emekciceyhun@users.noreply.github.com>
2022-09-16 16:07:40 +0300

    Veri Tipleri

---
+++ veriTiplerii/src/veriTiplerii/Main.java
@@ -0,0 +1,14 @@
+package veriTiplerii;
+
+public class Main {
+	public static void main(String[] args) {
+	int sayi =12;
+	double sayi2 = 13.2;
+	char karakter ='A';
+	boolean dogruMu = false;
+	System.out.println(sayi);
+	System.out.println(sayi2);
+	System.out.println(karakter);	
+	System.out.println(dogruMu);
+	}
+}
