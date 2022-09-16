196bf851412eb0a9642f97943d7d29cbb58e3734
emekciceyhun <47597251+emekciceyhun@users.noreply.github.com>
2022-09-16 15:50:19 +0300

    Degiskenler
    Degisken Atama

---
+++ Degiskenler/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int denemeSayisi = 5;
+		String mesaj = "Deneme";
+		System.out.println("Deneme Sayım : " +denemeSayisi);
+
+		
+	
+		
+		
+
+	}
+
+}
