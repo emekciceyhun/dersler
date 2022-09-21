2053b1fb0e12ce53f0f1cb81f95acb9e32c8bd01
emekciceyhun <47597251+emekciceyhun@users.noreply.github.com>
2022-09-21 20:44:23 +0300

    string2

---
+++ stringDemo2/src/stringDemo2/Main.java
@@ -0,0 +1,20 @@
+package stringDemo2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "	Bugün hava iyi	";
+		System.out.println(mesaj);
+		String yeniMesaj = mesaj.replace(' ', '-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2, 5));
+for (String kelime : mesaj.split(" ")) {
+	System.out.println(kelime);
+}
+System.out.println(mesaj.toLowerCase());
+System.out.println(mesaj.toUpperCase());
+
+System.out.println(mesaj.trim());
+	}
+
+}
