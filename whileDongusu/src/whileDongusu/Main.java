f3b2739a439fe0a1a18c770bfc9d2cb33b5e859d
emekciceyhun <47597251+emekciceyhun@users.noreply.github.com>
2022-09-21 14:44:19 +0300

    do-whileDongusu

--- whileDongusu/src/whileDongusu/Main.java
+++ whileDongusu/src/whileDongusu/Main.java
@@ -3,11 +3,20 @@ package whileDongusu;
 public class Main {
 
 	public static void main(String[] args) {
-		int i=1;
+		int i=0;
 		while(i<10) {
 System.out.println(i);
-	i++;
+	i+=2;
 		}
 System.out.println("while döngüsü bitti");
+
+int j=100;
+do {
+	System.out.println(j);
+j+=2;
+
+}while(j<10);
+
+System.out.println("do-while döngüsü bitti");
 }
 }
