3c5387f44c387b661f961c8b8e7683c1ccef453d
emekciceyhun <47597251+emekciceyhun@users.noreply.github.com>
2022-10-05 17:36:29 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/CustomerManager.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+import java.io.Console;
+
+public class CustomerManager {
+	public void List() {
+		System.out.println("Müşteri listelendi");
+	}
+	public void Add() {
+		System.out.println("Eklendi");
+}
+}
---
+++ inheritance/src/inheritance/EmployeeManager.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class EmployeeManager extends PersonManager {
+	public void List() {
+		System.out.println("Müşteri listelendi");
+	}
+
+	public void Add() {
+		System.out.println("Eklendi");
+	}
+
+	public void BestEmployee() {
+		System.out.println("Ayın elemanı getirildi");
+	}
+}
